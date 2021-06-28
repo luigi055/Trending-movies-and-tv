8f61c216dbe8aff82028ee2963f8e6c174609735
Pedro La Rosa <pedrolarosa@cornerjob.com>
2021-06-28 14:10:11 +0200

    basic flow working

--- src/components/stateful/header/header.tsx
+++ src/components/stateful/header/header.tsx
@@ -1,8 +1,11 @@
 import { logout } from "services/redux/features/authentication/actions";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser } from "services/redux/features/authentication/selectors";
 
 function Header() {
+	const user = useSelector(selectUser);
 	const dispatch = useDispatch();
+
 	const handleLogoutClick = async () => {
 		dispatch(logout());
 	};
@@ -10,6 +13,12 @@ function Header() {
 	return (
 		<header>
 			<button onClick={handleLogoutClick}>sign out</button>
+
+			<div>
+				<img src={user?.avatar} alt={`${user?.name}'s avatar`} />
+				<h3>{user?.name}</h3>
+				<h4>{user?.email}</h4>
+			</div>
 		</header>
 	);
 }
--- src/views/dashboard/dashboard.tsx
+++ src/views/dashboard/dashboard.tsx
@@ -1,16 +1,10 @@
 import Header from "components/stateful/header";
-import jwt from "jsonwebtoken";
-import { getCookie } from "services/cookies";
 
 const Dashboard = () => {
-	const user = jwt.decode(getCookie("access_token")) as IUser;
-	console.warn(user);
-
 	return (
 		<div>
 			<Header />
 			<h1>Dashboard</h1>
-			<main>{user.email}</main>
 		</div>
 	);
 };
